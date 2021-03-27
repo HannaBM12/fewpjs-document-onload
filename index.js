fa7cf199c1468621befe7f3f14a5e25de8c2eb12
HannaBM12 <Hannabmulugeta@gmail.com>
2021-03-27 15:34:36 -0400

    commit tree 226d82e4c986fa368e06b17195b68e46e598a672

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+// document.addEventListener('DOMContentLoaded', function(){
+//     console.log("DOM is Loaded")
+// })
+// console.log(
+//     "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+// )
+
+document.addEventListener("DOMContentLoaded", function(){
+    let pharagraph = document.querySelector("#text")
+    pharagraph.textContent = "This is really cool!"
+
+})
